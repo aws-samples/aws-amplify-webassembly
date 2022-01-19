8ebaf7fddb5d90d39941ee6215f6751b239d6630
Richard Neil Ilagan <ilagan@amazon.com>
2022-01-19 19:36:12 +0800

    feat: adds WASM SHA-256 integration

--- webapp/src/components/hash-card/hash-card.tsx
+++ webapp/src/components/hash-card/hash-card.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+
+import useHasher from '../../hooks/use-hasher'
 
 // :: ---
 
@@ -7,9 +9,20 @@ type HashCardProps = {
 }
 
 const HashCard: FC<HashCardProps> = () => {
+  // :: The useHasher hook interfaces with our WASM module.
+  const hasher = useHasher()
+
+  const [input, setInput] = useState<string>()
+  const [hashed, setHashed] = useState<string>('')
+
+  useEffect(() => {
+    if (!input) return
+    hasher(input).then(setHashed)
+  }, [input])
+
   return (
-    <div className='hash-card p-6 rounded shadow-lg w-1/3 bg-slate-50 flex flex-col gap-4'>
-      <header className='flex flex-col gap-2 w-2/3'>
+    <div className='hash-card p-6 rounded shadow-lg w-full min-w-fit bg-slate-50 flex flex-col gap-4'>
+      <header className='flex flex-col gap-2'>
         <h2 className='text-xl font-semibold'>SHA-256 hash calculator</h2>
         <p className='text-sm text-slate-600'>
           This uses a WebAssembly (WASM) module to calculate the SHA-256 hash of your input text
@@ -22,13 +35,19 @@ const HashCard: FC<HashCardProps> = () => {
           <textarea
             className='w-full p-2 outline-none focus:border-orange-400 border-2 text-blue-600'
             placeholder='Input your text here.'
+            value={input}
+            onChange={({ target }) => setInput(target.value)}
           ></textarea>
         </label>
       </div>
 
       <div>
         <header className='text-lg font-medium text-slate-500'>Hash</header>
-        <p>blah blah blah blah</p>
+        {hashed ? (
+          <p className='max-w-full font-mono text-purple-500'>{hashed}</p>
+        ) : (
+          <p>Type some text above, and the SHA-256 hash will appear here.</p>
+        )}
       </div>
 
       <footer></footer>
---
+++ webapp/src/hooks/use-hasher.tsx
@@ -0,0 +1,26 @@
+import { useCallback } from 'react'
+
+// :: These imports are available in the build context through npm link / yarn link.
+//    Refer to the Makefile in this repository to see how this is achieved.
+import init, { sha256 } from '@wasm-amplify-builds/wasm'
+
+// :: ?url is required so that the bundler doesn't attempt to mangle the WASM binary contents.
+import wasm from '@wasm-amplify-builds/wasm/wasm_bg.wasm?url'
+
+// :: ---
+
+const __compiled = init(wasm)
+
+const useHasher = () => {
+  const hasher = useCallback(
+    async (input) => {
+      await __compiled
+      return sha256(input)
+    },
+    [__compiled]
+  )
+
+  return hasher
+}
+
+export default useHasher

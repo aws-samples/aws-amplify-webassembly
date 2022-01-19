681b6739254725ce800ee2aa362e80c595dcd347
Richard Neil Ilagan <ilagan@amazon.com>
2022-01-19 22:32:18 +0800

    fix: corrects namespacing of wasm imports

--- webapp/src/hooks/use-hasher.tsx
+++ webapp/src/hooks/use-hasher.tsx
@@ -2,10 +2,10 @@ import { useCallback } from 'react'
 
 // :: These imports are available in the build context through npm link / yarn link.
 //    Refer to the Makefile in this repository to see how this is achieved.
-import init, { sha256 } from '@wasm-amplify-builds/wasm'
+import init, { sha256 } from '@wasm-amplify-build/wasm'
 
 // :: ?url is required so that the bundler doesn't attempt to mangle the WASM binary contents.
-import wasm from '@wasm-amplify-builds/wasm/wasm_bg.wasm?url'
+import wasm from '@wasm-amplify-build/wasm/wasm_bg.wasm?url'
 
 // :: ---
 
